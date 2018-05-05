8b55293647a01547bcb562ebb7806143963813f5
LittleLightCz <antonin.brettsnajdr@gmail.com>
2018-05-05 09:59:53 +0200

    Updated maven and README

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # NeuralSound
-Sound generator based on neural network
+Sound generator based on neural networks
+
+## Project goals
+Develop a sound generator that trains the neural network according to
+existing WAV file (sample) and enable to use this neural network to
+generate a new sound of any length based on the patterns it learned.
--- src/main/java/com/svetylkovo/neuralsound/wav/WavFileException.kt
+++ src/main/java/com/svetylkovo/neuralsound/wav/WavFileException.kt
@@ -1,11 +1,3 @@
 package com.svetylkovo.neuralsound.wav
 
-class WavFileException : Exception {
-    constructor() : super() {}
-
-    constructor(message: String) : super(message) {}
-
-    constructor(message: String, cause: Throwable) : super(message, cause) {}
-
-    constructor(cause: Throwable) : super(cause) {}
-}
+class WavFileException(message: String) : Exception(message)
