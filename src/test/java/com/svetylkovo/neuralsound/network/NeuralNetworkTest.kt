ecbbc2387aff57fe2a71abde88d6e2a2ffc18805
LittleLightCz <antonin.brettsnajdr@gmail.com>
2018-05-08 10:15:26 +0200

    Added test to N to N samples generation

--- src/test/java/com/svetylkovo/neuralsound/network/NeuralNetworkTest.kt
+++ src/test/java/com/svetylkovo/neuralsound/network/NeuralNetworkTest.kt
@@ -77,4 +77,69 @@ class NeuralNetworkTest {
         clipboard.setContents(selection, selection)
 
     }
+
+    @Test
+    fun neuralNetworkTestNtoNSamplesGeneration() {
+
+        println("Testing many-to-many neural network  ...")
+
+        val input = ((-10 until 10).map { it / 10.0 } + (10 downTo -9).map { it / 10.0 }).toMutableList()
+        input += input
+
+        val inputSize = 20
+        val hiddenSize = 2*inputSize+1
+        val outputSize = inputSize
+
+        val network = BasicNetwork().apply {
+            addLayer(BasicLayer(null, true, inputSize))
+            addLayer(BasicLayer(ActivationSigmoid(), true, hiddenSize))
+//            addLayer(BasicLayer(ActivationSigmoid(), true, hiddenSize / 2))
+            addLayer(BasicLayer(ActivationSigmoid(), false, outputSize))
+
+            structure.finalizeStructure()
+            reset()
+
+            val trainingMap = input.asSequence()
+                .map { it.toNormalized() }
+                .windowed(inputSize, inputSize)
+                .windowed(2, 1)
+                .map {
+                     it.first().toDoubleArray() to it.last().toDoubleArray()
+                }.toMap()
+
+            val dataSet = BasicMLDataSet(trainingMap.keys.toTypedArray(), trainingMap.values.toTypedArray())
+
+            val train = ResilientPropagation(this, dataSet, 0.1, 0.0001)
+
+            println("Learning ... (data size: ${dataSet.size()})")
+
+            var epoch = 1
+
+            do {
+                train.iteration()
+                println("Epoch ${epoch++}, error ${train.error}")
+            } while (train.error > 0.0001)
+
+            train.finishTraining()
+
+            println("Neural network ready!")
+        }
+
+        //output test
+        val result = input.take(inputSize).map { it.toNormalized() }.toMutableList()
+        val neuralOutputResult = DoubleArray(outputSize)
+
+        repeat((input.size / inputSize) * 4) {
+            network.compute(result.takeLast(inputSize).toDoubleArray(), neuralOutputResult)
+            result += neuralOutputResult.toList()
+        }
+
+        val unNormalizedResult = result.map { it.fromNormalized() }
+        unNormalizedResult.forEach { println(it) }
+
+        val selection = StringSelection(unNormalizedResult.joinToString("\n"))
+        val clipboard = Toolkit.getDefaultToolkit().getSystemClipboard()
+        clipboard.setContents(selection, selection)
+
+    }
 }
